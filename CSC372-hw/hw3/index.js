92c4ef319de3a14efb7809a7c78d6a77e391fe2d
Ethan Moore <emmoore4@uncg.edu>
2024-09-25 19:37:20 -0400

    Finished HW3

--- CSC372-hw/hw3/index.js
+++ CSC372-hw/hw3/index.js
@@ -1,29 +1,71 @@
 "use strict";
 
+/*
+  Name: Ethan Moore 
+  Date: 09.25.2024
+  CSC 372-01
+
+  This file defines all scripting for index.html.
+*/
+
+//Nav button functionality
 document.getElementById("homeBttn").addEventListener("click", () => {window.location.href = "index.html"});
 document.getElementById("mealPlanBttn").addEventListener("click", () => {window.location.href = "meal-plan.html"});
 
+//get section references
 let asideSections = document.querySelectorAll("aside > section");
 let articleSections = document.querySelectorAll("article > section");
 
-for (let i = 0; i < asideSections.length; i++) {
-    let section = asideSections[i];
+initializeDishImg();
 
-    section.addEventListener("click", (event) => {
-        extendSection(event);
-    });
-    section.setAttribute("id", "s" + i);
-};
+initializeLocationSections();
+
 
 /**
- * 
+ * Properly extends and shows the selected location. 
  * @param {Event} event 
  */
 function extendSection(event) {
     let targetId = new String(event.currentTarget.id);
-    let target = document.getElementById(targetId);
     let idNum = Number.parseInt(targetId.slice(1));
 
     let targetArticleSec = articleSections.item(idNum);
-    targetArticleSec.classList.toggle("extend");
+    if (targetArticleSec.classList.contains("extend")) {
+        targetArticleSec.classList.remove("extend");
+    } else {
+        document.querySelectorAll(".extend").forEach((el) => {el.classList.remove("extend")});
+        targetArticleSec.classList.add("extend");
+    }
+}
+
+/**
+ * Initializes Dish Image functionality in index.html.
+ */
+function initializeDishImg() {
+    document.querySelectorAll(".dish-img-gallery > img").forEach(
+        (img) => {
+            img.addEventListener("click", (event) => {
+                document.querySelectorAll(".dish-desc-display").forEach((el) => {el.classList.remove("dish-desc-display")});
+                document.querySelectorAll("img.selected").forEach((el) => {el.classList.remove("selected")});
+                let targetId = event.currentTarget.id;
+                document.getElementById(targetId + "Desc").classList.add("dish-desc-display");
+                document.getElementById(targetId).classList.add("selected");
+            });
+        }
+    );
+}
+
+
+/**
+ * Initializes functionality for location sections.
+ */
+function initializeLocationSections() {
+    for (let i = 0; i < asideSections.length; i++) {
+        let section = asideSections[i];
+    
+        section.addEventListener("click", (event) => {
+            extendSection(event);
+        });
+        section.setAttribute("id", "s" + i);
+    };
 }
--- CSC372-hw/hw3/meal-plan.js
+++ CSC372-hw/hw3/meal-plan.js
@@ -1,33 +1,88 @@
 "use strict";
 
+/*
+  Name: Ethan Moore 
+  Date: 09.25.2024
+  CSC 372-01
+
+  This file defines all scripting for meal-plan.html.
+*/
+
+//Nav button functionality
 document.getElementById("homeBttn").addEventListener("click", () => {window.location.href = "index.html"});
 document.getElementById("mealPlanBttn").addEventListener("click", () => {window.location.href = "meal-plan.html"});
 
+initializeButtons();
+
+//meal variables
 let mealCount = new Map();
 let totalCost = 0;
+let mealsContainerRef = document.getElementById("meals");
 
-function addMeal(mealNum) {
+/**
+ * Adds meal to meal plan.
+ * @param {Event} event
+ */
+function addMeal(event) {
+    let mealNum = event.currentTarget.id.slice(3);
     let mealValRef = document.getElementById("meal" + mealNum + "Val");
-    let mealName = document.getElementById("meal" + mealNum + "Name");
+    let mealNameRef = document.getElementById("meal" + mealNum + "Name");
     let mealValString = mealValRef.textContent.slice(1);
     let mealCost = Number.parseFloat(mealValString);
 
-    addToMealPlan(mealNum, mealName, mealValRef.textContent);
+    addToMealPlan(mealNum, mealNameRef.textContent, mealValRef.textContent);
 
     totalCost += mealCost;
     updateTotal();
 }
 
+/**
+ * Helper method for {@link addMeal}.
+ * @param {number} mealNum 
+ * @param {string} mealName 
+ * @param {string} mealVal 
+ */
 function addToMealPlan(mealNum, mealName, mealVal) {
     if (mealCount.has(mealNum)) {
-        mealCount.set(mealNum, mealCount.get(mealNum)++);
+        mealCount.set(mealNum, mealCount.get(mealNum) + 1);
     } else {
         mealCount.set(mealNum, 1);
     }
 
-    //Todo
+    buildMealElement(mealNum, mealName, mealVal);
 }
 
+/**
+ * Helper method for {@link addToMealPlan} that formats and displays the meal element in
+ * the meal plan.
+ * @param {number} mealNum 
+ * @param {string} mealName 
+ * @param {string} mealVal 
+ */
+function buildMealElement(mealNum, mealName, mealVal) {
+    if (mealCount.get(mealNum) > 1) {
+        document.getElementById("planMealCount" + mealNum).textContent = mealCount.get(mealNum);    
+    } else {
+        let mealContainerRef = document.createElement("div");
+        mealContainerRef.id = "planMeal" + mealNum;
+
+        let planMealInfoRef = document.createElement("p");
+        planMealInfoRef.textContent = `${mealName}: ${mealVal} * `;
+
+        let planMealCountRef = document.createElement("span");
+        planMealCountRef.id = "planMealCount" + mealNum;
+        planMealCountRef.textContent = mealCount.get(mealNum);
+
+        planMealInfoRef.appendChild(planMealCountRef);
+        mealContainerRef.appendChild(planMealInfoRef);
+        mealsContainerRef.appendChild(mealContainerRef);
+    }
+    
+}
+
+/**
+ * Helper method for {@link addMeal} that updates the total in the meal plan.
+ */
 function updateTotal() {
     let newCostString = "0.00";
     let totalCostRef = document.getElementById("totalAmount");
@@ -42,4 +97,14 @@ function updateTotal() {
     }
 
     totalCostRef.textContent = newCostString;
+}
+
+/**
+ * Initializes add button functionality
+ */
+function initializeButtons() {
+    let bttns = document.querySelectorAll(".featured-dish > button");
+    bttns.forEach((bttn) => {
+        bttn.addEventListener("click", addMeal);
+    });
 }
