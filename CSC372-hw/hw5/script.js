29ca7d509539b4e261d5dd6fa1630627da2e1ea6
Ethan Moore <emmoore4@uncg.edu>
2024-10-22 15:05:41 -0400

    commenting

--- CSC372-hw/hw5/script.js
+++ CSC372-hw/hw5/script.js
@@ -1,4 +1,13 @@
 "use strict";
+
+/*
+Name: Ethan Moore
+Date: 10.22.2024
+CSC 372-01
+
+This file contains the scripting for index.html in homework 5, GitHub Gallery.
+*/
+
 const FETCH_REPO_URI = "https://api.github.com/users/$/repos";
 const DEFAULT_USER = "emoore5931";
 const CARD_TEMPLATE = document.querySelector("#githubCard");
@@ -8,17 +17,15 @@ let requestUser = DEFAULT_USER;
 GAL_ID_REF.textContent = requestUser;
 
 getRepos(requestUser).then((repoArr) => {
-    let cards = [];
     repoArr.forEach((repo) => {
-        cards.push(repoToCard(repo));
+        repoToCard(repo).then((card) => {
+            displayCard(card, document.getElementById("container1"));
+        });
     });
-    cards.forEach((card) => {
-        displayCard(card, document.getElementById("container1"));
-    });
-})
+}, () => {console.error("Failed to get repos")});
 
 /**
- * 
+ * Object that contains GitHub repo data to be displayed.
  * @param {string} name 
  * @param {string} desc 
  * @param {number} numWatchers 
@@ -40,9 +47,9 @@ function GithubCard(name, desc, numWatchers, numCommits, langs, createDate, upda
 }
 
 /**
- * 
- * @param {GithubCard} githubCard 
- * @param {HTMLElement} containerRef
+ * Builds a reactive card element to append to the specified container.
+ * @param {GithubCard} githubCard github card object that contains all repo information
+ * @param {HTMLElement} containerRef reference to the card container
  */
 function displayCard(githubCard, containerRef) {
     let card = CARD_TEMPLATE.content.cloneNode(true);
@@ -60,10 +67,15 @@ function displayCard(githubCard, containerRef) {
     containerRef.appendChild(card);
 }
 
+/**
+ * Fetches and returns objects containing GitHub repo data from a specified repo owner.
+ * @param {string} ownerName repo owner GitHub username
+ * @returns {Array} array of repo objects
+ */
 async function getRepos(ownerName) {
     const repoArr = [];
 
-    fetch(FETCH_REPO_URI.replace("$", ownerName), {
+    await fetch(FETCH_REPO_URI.replace("$", ownerName), {
         method: "GET"
     })
     .then((response) => response.json()).then((data) => {
@@ -75,6 +87,42 @@ async function getRepos(ownerName) {
     return repoArr;
 }
 
-function repoToCard(repo) {
-    return new GithubCard(repo.name, repo.description, repo.watchers_count, 0, repo.language, repo.created_at, repo.updated_at, repo.html_url)
+/**
+ * Function to convert a repo object to a card object.
+ * @param {Object} repo repo object
+ * @returns {GithubCard}
+ */
+async function repoToCard(repo) {
+    let description;
+    (repo.description == null || repo.description.length == 0) ? description = "No description" : description = repo.description;
+    
+    let language;
+    //BUG HERE
+    (typeof repo.language === 'null' || repo.language == null || repo.language.length == 0) ? lanugage = "N/A" : language = repo.language;
+
+    const createDate = new Date(repo.created_at).toLocaleString();
+    const updateDate = new Date(repo.updated_at).toLocaleString();
+    const numCommits = await getCommitCount(repo.commits_url);
+
+    console.log(numCommits);
+
+    return new GithubCard(repo.name, description, repo.watchers_count, numCommits, language, createDate, updateDate, repo.html_url)
+}
+
+/**
+ * Helper method that fetches the commit count for {@link repoToCard}
+ * @param {string} commitURL the URL pointing to GitHub API to pull commit data
+ * @returns {number} the resulting number of commits
+ */
+async function getCommitCount(commitURL) {
+    let numCommits = await fetch(commitURL.replace("{/sha}", ""), {
+        method: "GET"
+    })
+    .then((response) => {
+        response.json().then((commitArr) => {
+            return commitArr.length;
+        })
+    });
+    
+    return numCommits;
 }
